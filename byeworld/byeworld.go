85f3f6106538d545dbe3348be7a6a85e46d1e21e
Tamara Kaufler <tamara@weave.works>
2017-11-27 14:05:04 +0000

    First commit

---
+++ byeworld/byeworld.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var rev = "devel"
+
+func main() {
+	var (
+		addr = flag.String("addr", ":3000", "HTTP listen address")
+		msg  = flag.String("msg", "Bye, my good old world; welcome terrifying new world", "Message to print")
+	)
+
+	flag.Parse()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+		fmt.Fprintf(w, *msg+"\n")
+	})
+	log.Printf("%s: listening on %s", rev, *addr)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
